b3db93d5bd5920f48b54125392ea49be861ec275
Eduard Bonada <eduard.bonada@gmail.com>
2016-05-19 13:05:59 +0200

    Create object representation of schedule + create text schedule for day X

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 //var express  	= require('express');
 //var app 	 	= express();
 var mongoose 	= require('mongoose');
+var moment  	= require('moment');
 
 var config = require('./config');
 
@@ -19,7 +20,7 @@ mongoose.connect(config.database, function(err) {
 	//setMustBandsForUser('test_user_1', ['interpol', 'thestrokes']);
 	//computeSimToMustBandsForUser('test_user_1');
 
-	computeSchedule('test_user_1')
+	computeScheduleForDay('test_user_1', '29/05/2015')
 
 });
 
@@ -78,13 +79,13 @@ function computeSimToMustBandsForUser(userName){
 
 }
 
-/// ---------- COMPUTE SCHEDULE ---------- ///
+/// ---------- SCHEDULE ---------- ///
 
-function computeSchedule(userName) {
+function computeScheduleForDay(userName, day) {
+
+	// day in the format 'dd/mm/yyyy'
 
 	var schedule = require('./schedule')
-
-	var bandsToAttend = [];
 
 	// get user
 	User.findOne(
@@ -103,8 +104,20 @@ function computeSchedule(userName) {
 						bandsDict[bands[b].lowercase] = bands[b];
 					}
 					
-					bandsToAttend = schedule.generateSchedule(user, bandsDict);
-					console.log("[SERVER] Bands to attend: " + bandsToAttend.join(' '));
+					var bandsToAttend = schedule.generateSchedule(user, bandsDict);
+					
+					var objectSchedule = objectRepresentationOfSchedule(
+											bandsToAttend, 
+											bandsDict);
+					//console.log(objectSchedule);
+
+					var textSchedule = textRepresentationOfScheduleInRange(
+											objectSchedule, 
+											{
+												start: moment(day + " 12:00", "D/MM/YYYY HH:mm"),
+												end: moment(day + " 12:00", "D/MM/YYYY HH:mm").add(1, 'days')
+											});
+					console.log(textSchedule);
 
 				}
 			);
@@ -113,3 +126,73 @@ function computeSchedule(userName) {
 	);
 
 }
+
+function objectRepresentationOfSchedule(bandsToAttend, bandsInfo){
+	
+	var scheduleObject = [];
+
+	// Gather info for all bands to attend 
+	for(b in bandsToAttend){
+
+		var bandName = bandsToAttend[b];
+
+		var bandObj = {
+			lowercase: bandsInfo[bandName]["lowercase"],
+			uppercase: bandsInfo[bandName]["uppercase"],
+			start: bandsInfo[bandName]["startTime"],
+			end: bandsInfo[bandName]["endTime"],
+			stage: bandsInfo[bandName]["stage"]	
+		};
+
+		scheduleObject.push(bandObj);
+		
+	}
+
+	// Sort by start time 
+	var scheduleObjectSortedByStartTime = scheduleObject.sort(function(a,b) {
+
+		if ( moment(a.start).isAfter(moment(b.start)) ){
+			return 1;
+		}
+		else if (moment(a.start).isBefore(moment(b.start)) ){
+			return -1;
+		}
+		else{
+			return 0;
+		}
+
+	}); 
+
+
+	return(scheduleObjectSortedByStartTime);
+}
+
+function textRepresentationOfScheduleInRange(objectSchedule, momentsRange){
+
+	var textSchedule = "";
+
+	for(b in objectSchedule){
+
+		var bandStart = objectSchedule[b]["start"];
+
+		if( moment(bandStart).isAfter(momentsRange.start) && moment(bandStart).isBefore(momentsRange.end) ) {
+
+			textSchedule += objectSchedule[b]["uppercase"] + " (" + moment(objectSchedule[b]["start"]).format("D HH:mm") + " " + objectSchedule[b]["stage"] + ")\n";
+		}
+
+	}
+	
+	return(textSchedule);
+
+}
+	
+
+
+
+
+
+
+
+
+
+
